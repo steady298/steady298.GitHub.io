9b1196f1c70bded2169211922d192a98dbded4db
steady298 <115759238+steady298@users.noreply.github.com>
2022-10-15 00:34:52 +0800

    Update constants.ts

--- docs/.vitepress/config/constants.ts
+++ docs/.vitepress/config/constants.ts
@@ -3,8 +3,8 @@ const site = 'https://blog.charles7c.top'
 export const metaData = {
   lang: 'zh-CN',
   locale: 'zh_CN',
-  title: '查尔斯的知识库',
-  description: '个人知识库，记录 & 分享个人碎片化、结构化、体系化的知识内容。',
+  title: 'Coolman的知识库',
+  description: '个人知识库，学习记录 & 分享个人学习、生活、以及一些零碎的东西。',
   site,
   image: `${site}/logo.jpg`
 }
