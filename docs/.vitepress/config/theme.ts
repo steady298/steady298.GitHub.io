54e6432bf2b5d44c14105e1e3d1f5ff2b2833ba0
steady298 <115759238+steady298@users.noreply.github.com>
2022-10-15 14:07:42 +0800

    Update theme.ts

--- docs/.vitepress/config/theme.ts
+++ docs/.vitepress/config/theme.ts
@@ -17,7 +17,7 @@ export const themeConfig: DefaultTheme.Config = {
   },
   // 编辑链接配置
   editLink: {
-    pattern: 'https://github.com/Charles7c/charles7c.github.io/edit/main/repos/:path',
+    pattern: 'https://github.com/steady298/steady298.GitHub.io/edit/main/repos/:path',
     text: '不妥之处，敬请雅正'
   },
   // 全文搜索配置
@@ -28,7 +28,7 @@ export const themeConfig: DefaultTheme.Config = {
   },
   // 导航栏右侧社交链接配置
   socialLinks: [
-    { icon: 'github', link: 'https://github.com/Charles7c/charles7c.github.io' },
+    { icon: 'github', link: 'https://github.com/steady298/steady298.GitHub.io' },
     {
       icon: {
         svg: '<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>码云</title><path d="M11.984 0A12 12 0 0 0 0 12a12 12 0 0 0 12 12 12 12 0 0 0 12-12A12 12 0 0 0 12 0a12 12 0 0 0-.016 0zm6.09 5.333c.328 0 .593.266.592.593v1.482a.594.594 0 0 1-.593.592H9.777c-.982 0-1.778.796-1.778 1.778v5.63c0 .327.266.592.593.592h5.63c.982 0 1.778-.796 1.778-1.778v-.296a.593.593 0 0 0-.592-.593h-4.15a.592.592 0 0 1-.592-.592v-1.482a.593.593 0 0 1 .593-.592h6.815c.327 0 .593.265.593.592v3.408a4 4 0 0 1-4 4H5.926a.593.593 0 0 1-.593-.593V9.778a4.444 4.444 0 0 1 4.445-4.444h8.296Z"/></svg>'
@@ -39,13 +39,13 @@ export const themeConfig: DefaultTheme.Config = {
       icon: {
         svg: '<svg role="img" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg"><title>查尔斯的网址导航</title><path d="M512 22C241.38 22 22 241.38 22 512s219.38 490 490 490 490-219.38 490-490S782.62 22 512 22z m226.06 277.73L517.54 809.14c-3.18 7.28-13.72 7.08-17.21-0.41L410.4 616.57a5.966 5.966 0 0 0-2.97-2.97L215.6 523.58c-7.58-3.49-7.79-14.05-0.41-17.23l508.74-220.76c8.91-3.91 18.03 5.22 14.13 14.14z" p-id="4967"></path></svg>'
       },
-      link: 'https://nav.charles7c.top'
+      link: 'https://github.com/steady298/steady298.GitHub.io'
     }
   ],
 
   // 自定义扩展: 文章元数据配置
   articleMetadataConfig: {
-    author: '查尔斯', // 文章全局默认作者名称
+    author: 'Coolman', // 文章全局默认作者名称
     authorLink: '/about/me', // 点击作者名时默认跳转的链接
     showPv: false // 是否显示文章阅读数, 需配置好相应后端API接口
   },
